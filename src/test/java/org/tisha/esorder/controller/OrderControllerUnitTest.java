09e37e27ff27480b4f3246dc05aad7eb72203efe
Tisha <tisha.kuprevich@gmail.com>
2022-12-10 14:58:47 +0400

    first tests

---
+++ src/test/java/org/tisha/esorder/controller/OrderControllerUnitTest.java
@@ -0,0 +1,31 @@
+package org.tisha.esorder.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(OrderController.class)
+@ExtendWith(SpringExtension.class)
+public class OrderControllerUnitTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void testGetOrders() throws Exception {
+        mockMvc.perform(get("/orders"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$", Matchers.hasSize(3)));
+    }
+}
